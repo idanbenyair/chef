e332fd3c69951e0aa149918888a7433a7ee15877
Idan <idan.benyair@live.com>
2017-05-10 18:43:35 +0000

    Minor change the the default.rb recipe

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -1,18 +1,19 @@
 #
-# Cookbook:: apache
-# Recipe:: default
+## Cookbook:: apache
+## Recipe:: default
+##
+## Copyright:: 2017, The Authors, All Rights Reserved.
 #
-# Copyright:: 2017, The Authors, All Rights Reserved.
 
 if node['platform_family'] == "rhel"
-	package = "httpd"
-elsif node['platform_family'] == "debian"
-	package = "apache2"
+        package = "httpd"
+        elsif node['platform_family'] == "debian"
+        package = "apache2"
 end
 
 package 'apache2' do
 	package_name package
-	action :install
+        action :install
 end
 
 service 'apache2' do
